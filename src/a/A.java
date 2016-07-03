4e62e14451b534f4df2786e279504c1492602c9d
Davy <Davy@192.168.1.4>
2016-07-02 23:21:38 -0300

    Versão 1.1

--- src/a/A.java
+++ src/a/A.java
@@ -23,7 +23,6 @@ public class A {
     public int getA1() {
         return A1;
     }
-
     public void setA1(int A1) {
         this.A1 = A1;
     }
@@ -31,7 +30,6 @@ public class A {
     public float getA2() {
         return A2;
     }
-
     public void setA2(float A2) {
         this.A2 = A2;
     }
@@ -39,7 +37,6 @@ public class A {
     public String getA3() {
         return A3;
     }
-
     public void setA3(String A3) {
         this.A3 = A3;
     }
